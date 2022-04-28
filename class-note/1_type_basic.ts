fd3212ceec9696205861d90b0b8d73b36cc557f4
park ji seong <70843882+lty1362@users.noreply.github.com>
2022-04-28 15:19:23 +0900

    Add files via upload

---
+++ class-note/1_type_basic.ts
@@ -0,0 +1,31 @@
+// JS 문자열 선언
+// var STR = 'hello';
+
+// TS 문자열
+let STR: string = 'hello';
+
+// TS 숫자
+let NUM: number = 1;
+
+// TS 배열
+let ARR: Array<number> = [1, 2, 3];
+let HEROS: Array<string> = ['Capt', 'Thor', 'Hulk']
+let ITEMS: number[] = [1, 2, 3];
+
+// TS 튜플
+let ADDRESS: [string, number] = ['gangnam', 100];
+
+// TS 객체
+let OBJ: object = {};
+
+let person: { name: string, age: number} = {
+  name : 'thor',
+  age : 200
+};
+// let person: objcet = {
+//   name = 'capt',
+//   age = 100
+// };
+
+// TS 진위값
+let bool:boolean = false;
---
+++ class-note/2_function.ts
@@ -0,0 +1,17 @@
+// function sum(a: number, b: number): number {
+//   return a + b;
+// }
+
+// sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의
+function sum(): number {
+  return 10;
+}
+
+// 함수의 반환 값에 타입을 정의
+function sum(a: number, b: number): number {
+  return a + b;
+}
+
+sum(10, 20, 30, 40)
